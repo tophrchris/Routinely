555d5755f3bb3d462e6837cf0ff9d1a7521d26fe
Chris Hollander <chollander@newclassrooms.org>
2016-05-04 15:46:31 -0400

    project bootstrap

---
+++ clockking.Model/Model.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace clockking.Model
+{
+	public class CheckPoint
+	{
+		private List<Occurrence> occurrences;
+		public CheckPoint()
+		{
+			this.occurrences = new List<Occurrence> ();
+		}
+		public string Name { get; set; }
+		public string Color { get; set; }
+		public string Emoji { get; set; }
+		public TimeSpan TargetTime { get; set; }
+
+		public IEnumerable<Occurrence> Occurrences { get{return this.occurrences; }}
+
+		public void AddOccurrence(Occurrence newOccurance)
+		{
+			this.occurrences.Add (newOccurance);
+		}
+		public Occurrence CreateOccurrence()
+		{
+			return this.CreateOccurrence (DateTime.Now);	
+		}
+
+		public Occurrence CreateOccurrence(DateTime observationTimeStamp)
+		{
+			return new Occurrence(this,observationTimeStamp);
+		}
+		public TimeSpan averageObservedTime
+		{
+			get{
+				if (!occurrences.Any ())
+					return this.TargetTime;
+
+				var avgminutes = this.occurrences.Average (o => o.Time.TotalMinutes);
+				return TimeSpan.FromMinutes (avgminutes);
+			}
+		}
+		public override string ToString ()
+		{
+			return string.Format ("{3}{0}: target={1}, avg={2}", 
+				this.Name,
+				this.TargetTime,
+				this.averageObservedTime,
+				this.Emoji);
+		}
+	}
+
+	public class Occurrence
+	{
+
+		private CheckPoint checkPoint;
+
+		public DateTime timeStamp; 
+
+		public Occurrence(CheckPoint assignedCheckPoint,DateTime observedTime)
+		{
+			this.checkPoint = assignedCheckPoint;
+			this.timeStamp = observedTime;
+		}
+
+		public string checkpointLabel { get { return this.CheckPoint.Name; } }
+
+		[JsonIgnore]
+		public CheckPoint CheckPoint { get { return this.checkPoint; } }
+
+		[JsonIgnore]
+		public TimeSpan Time 
+		{
+			get
+			{
+				return this.timeStamp - this.timeStamp.Date;
+			}
+		}
+	}
+
+	public class CheckPointPair
+	{
+		private CheckPoint firstEvent;
+		private CheckPoint secondEvent;
+
+		public CheckPointPair(CheckPoint first, CheckPoint second)
+		{
+			this.firstEvent = first;
+			this.secondEvent = second;
+		}
+		public TimeSpan InterveningTime 
+		{
+			get{
+				return this.secondEvent.averageObservedTime - this.firstEvent.averageObservedTime;
+			}
+		}
+		public override string ToString ()
+		{
+			return string.Format ("{5}{0} at {1} then wait {2} for {3} at {4}",
+				this.firstEvent.Name,
+				this.firstEvent.averageObservedTime,
+				this.InterveningTime,
+				this.secondEvent.Name,
+				this.secondEvent.averageObservedTime,
+				this.firstEvent.Emoji);
+
+		}
+
+	}
+}
+
+
---
+++ clockkingConsolePlayground/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using clockking.Model;
+
+
+namespace clockkingConsolePlayground
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+
+
+			var checkPoints = new Dictionary<string,CheckPoint> () {
+				{"wakeup",new CheckPoint(){Name="wakeup",TargetTime=TimeSpan.FromHours(6)}},
+				{"breakfast",new CheckPoint (){ Name = "Breakfast",TargetTime= TimeSpan.FromHours(9) }},
+				{"lunch",new CheckPoint () { Name = "Lunch", TargetTime=TimeSpan.FromHours(12)}},
+				{"dinner",new CheckPoint(){Name="dinner",TargetTime=TimeSpan.FromHours(19)}},
+				{"bedtime",new CheckPoint(){Name="bedtime",TargetTime=TimeSpan.FromHours(23)}}
+
+			};
+
+
+
+
+			var b = checkPoints ["breakfast"];
+			var l = checkPoints ["lunch"];
+
+			b.AddOccurrence (b.CreateOccurrence (DateTime.Parse("5/1/16 9:05am")));
+			b.AddOccurrence (b.CreateOccurrence (DateTime.Parse("5/2/16 9:15am")));
+			b.AddOccurrence (b.CreateOccurrence (DateTime.Parse("5/3/16 9:32am")));
+			l.AddOccurrence (l.CreateOccurrence (DateTime.Parse("5/3/16 11:32am")));
+			l.AddOccurrence (l.CreateOccurrence (DateTime.Parse("5/3/16 12:32pm")));
+			l.AddOccurrence (l.CreateOccurrence (DateTime.Parse("5/3/16 1:32pm")));
+
+
+			var ordered = checkPoints
+							.Select (kv => kv.Value)
+							.OrderBy (cp => cp.averageObservedTime)
+				.Select ((c, i) => new{index = i,checkpoint = c});
+
+			var first = ordered.First();
+
+			var paired = 
+				from e1 in ordered
+				join e2 in ordered on e1.index equals e2.index - 1 into gj
+				from oe in gj.DefaultIfEmpty (first)
+				select new CheckPointPair (e1.checkpoint, oe.checkpoint);
+
+			foreach(var p in paired)
+				Console.WriteLine (p);
+
+			//var allOccurances = checkPoints.SelectMany (kv => kv.Value.Occurrences);
+
+			string json = JsonConvert.SerializeObject (checkPoints, Formatting.Indented,
+				new JsonSerializerSettings{ReferenceLoopHandling=ReferenceLoopHandling.Ignore});
+			Console.WriteLine (json);
+		
+			Console.ReadLine ();
+				
+		}
+	}
+}
