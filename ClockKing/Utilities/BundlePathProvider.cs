74bb4acdf42e756f9baa9b175af93df023656bd8
Chris Hollander <chollander@newclassrooms.org>
2016-07-28 12:35:11 -0400

    adding bundle path provider and logo

---
+++ ClockKing/Utilities/BundlePathProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Foundation;
+using System.IO;
+
+
+
+namespace ClockKing
+{
+	public class BundlePathProvider:PathProvider
+	{
+		public BundlePathProvider():base(string.Empty)
+		{
+			var bundle = NSBundle.MainBundle;
+
+			var fileName = "examples.json";
+
+			this.MyDocumentsPath = bundle.ResourcePath;
+			this.CheckpointPath = Path.Combine(this.MyDocumentsPath, fileName);
+			this.OccurrencesPath = string.Empty;
+		}
+	}
+}
+
